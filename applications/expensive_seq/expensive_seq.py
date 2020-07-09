f8eb585a4f9e295a7ee3dc0c4fdcf02a294e2d21
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-07-09 07:42:55 -0700

    Complete expensive_seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,14 +1,22 @@
-# Your code here
+from applications.cache import cache
 
 
-def expensive_seq(x, y, z):
-    # Your code here
+@cache
+def expensive_seq(x: int, y: int, z: int) -> int:
+    if x <= 0:
+        return y + z
+
+    if x > 0:
+        return expensive_seq(x - 1, y + 1, z) + expensive_seq(x - 2, y + 2, z * 2) + expensive_seq(x - 3, y + 3, z * 3)
 
 
-
-if __name__ == "__main__":
+def main() -> None:
     for i in range(10):
         x = expensive_seq(i*2, i*3, i*4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
     print(expensive_seq(150, 400, 800))
+
+
+if __name__ == "__main__":
+    main()
