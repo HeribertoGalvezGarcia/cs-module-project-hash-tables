1f5d84af4356e21ffebeea670d64dfc3bd1a5d51
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-07-08 21:41:50 -0700

    Create crack_caesar

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -1,5 +1,24 @@
-# Use frequency analysis to find the key to ciphertext.txt, and then
-# decode it.
+with open('ciphertext.txt') as f:
+    text = f.read()
 
-# Your code here
+frequency_order = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U', 'G', 'F', 'B', 'M', 'Y', 'C', 'P',
+                   'K', 'V', 'Q', 'J', 'X', 'Z']
 
+
+def main() -> None:
+    filtered_text = ''.join(filter('ABCDEFGHIJKLMNOPQRSTUVWXYZ'.__contains__, text))
+
+    count = {}
+
+    for char in filtered_text:
+        try:
+            count[char] += 1
+        except KeyError:
+            count[char] = 1
+
+    sorted_count = sorted(count, key=count.get, reverse=True)
+    print(text.translate({ord(x): y for x, y in zip(sorted_count, frequency_order)}))
+
+
+if __name__ == '__main__':
+    main()
