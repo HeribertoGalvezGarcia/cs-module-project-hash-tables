adc6ac8902e831cec828161418abf421e219c291
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-07-08 21:08:06 -0700

    Complete word count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,16 @@
-def word_count(s):
-    # Your code here
+from typing import Dict
 
+
+def word_count(s: str) -> Dict[str, int]:
+    count = {}
+
+    for word in s.translate({ord(c): '' for c in r'":;,.-+=/\|[]{}()*^&'}).lower().split():
+        try:
+            count[word] += 1
+        except KeyError:
+            count[word] = 1
+
+    return count
 
 
 if __name__ == "__main__":
