c54e8440c3c82bc3b3cb8207a216cba3751c1d66
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-07-09 07:28:49 -0700

    Complete histo

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,21 @@
-# Your code here
+with open('robin.txt') as f:
+    text = f.read()
 
+
+def main() -> None:
+    words = {}
+    sanitized_text = text.translate({ord(c): '' for c in r'":;,.-+=/\|[]{}()*^&'}).lower().split()
+    longest_word = len(max(sanitized_text, key=len))
+
+    for word in sanitized_text:
+        try:
+            words[word] += 1
+        except KeyError:
+            words[word] = 1
+
+    print('\n'.join(f'{word: <{longest_word + 2}}{"#" * amount}'
+                    for word, amount in sorted(words.items(), key=lambda x: (-x[1], x[0]))))
+
+
+if __name__ == '__main__':
+    main()
