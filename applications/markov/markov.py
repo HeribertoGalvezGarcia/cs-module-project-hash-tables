d60a5f92b9985e9c4ac0ecf92926a7d5946dbe54
HeribertoGalvezGarcia <heriberto.galvezgarcia@gmail.com>
2020-07-09 22:38:24 -0700

    Complete markov

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,41 @@
 import random
+import re
 
-# Read in all the words in one go
+
 with open("input.txt") as f:
     words = f.read()
 
-# TODO: analyze which words can follow other words
-# Your code here
+starting_re = re.compile(r'^"?[A-Z].+')
 
 
-# TODO: construct 5 random sentences
-# Your code here
+def main() -> None:
+    follow_ups = {}
+    split_words = words.split()
 
+    for i, word in enumerate(split_words, 1):
+        try:
+            next_word = split_words[i]
+        except IndexError:
+            continue
+
+        try:
+            follow_ups[word].append(next_word)
+        except KeyError:
+            follow_ups[word] = [next_word]
+
+    for _ in range(10):
+        start_word = ''
+
+        while not starting_re.match(start_word):
+            start_word = random.choice(split_words)
+
+        sentence = [start_word]
+
+        while not sentence[-1].endswith(('.', '!', '?', '".', '"!', '"?')):
+            sentence.append(random.choice(follow_ups[sentence[-1]]))
+
+        print(' '.join(sentence))
+
+
+if __name__ == '__main__':
+    main()
